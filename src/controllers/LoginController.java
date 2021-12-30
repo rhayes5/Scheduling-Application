72aa1319337c306cf54a30c328e5fee4415441d0
shelly1138 <rychell.hayes@gmail.com>
2021-12-30 06:42:36 -0800

    Added date and an end time to login controller upcomingAppointmentAlert

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -84,7 +84,8 @@ public class LoginController implements Initializable {
                         upcomingAppointmentAlert.setContentText("There is an upcoming appointment in " +
                                 Duration.between(LocalTime.now(),
                                         appointment.getStartTime()).toMinutes() + " minute(s) " +
-                                "\n\nAppointment Time: " + appointment.getStartTime() +
+                                "\n\nDate: " + appointment.getStartDate() +
+                                "\nTime: " + appointment.getFormattedStart() + " - " + appointment.getFormattedEnd() +
                                 "\nCustomer: " + DBCustomers.getCustomerById(appointment.getCustomerId()).getName() +
                                 "\nContact: " + DBContacts.getContactById(appointment.getContactId()).getName() +
                                 "\nLocation: " + appointment.getLocation());
